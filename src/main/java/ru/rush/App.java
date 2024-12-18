7fa8946617cc320de44ddb446b20657d1eeec985
avfyodorov <avfyodorov60@yandex.ru>
2024-12-18 13:37:27 +0300

    add h2..

--- src/main/java/ru/rush/App.java
+++ src/main/java/ru/rush/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
+ * Herlo world!
  */
 @SpringBootApplication
 public class App {
