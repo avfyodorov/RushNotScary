3e4d1dce3b13c20aa9afcfb0a1d7c66c2e8b93e3
avfyodorov <avfyodorov60@yandex.ru>
2025-01-19 19:27:32 +0300

    fruit anntation

--- src/main/java/ru/rush/entities/Fruit.java
+++ src/main/java/ru/rush/entities/Fruit.java
@@ -1,17 +1,23 @@
 package ru.rush.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
 @Table(name ="fruit")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Fruit {
 
     @Id
     @Column(name = "id_fruit")
-    @GenericGenerator(name = "generator", strategy = "increment")
+    @GenericGenerator(name = "generator")
     @GeneratedValue(generator = "generator")
     private Integer id;
 
@@ -21,4 +27,19 @@ public class Fruit {
     @Column(name = "provider_code")
     private Integer providerCode;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Fruit fruit = (Fruit) o;
+        return getId() != null && Objects.equals(getId(), fruit.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
